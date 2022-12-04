2306392807f151ac430dbfde9cc5fca718c1560f
Sreeharie123 <sreehariengoli@gmail.com>
2022-12-04 18:41:59 +0530

    snake movement

--- snakeGame1.js
+++ snakeGame1.js
@@ -16,6 +16,7 @@ let yMove=0;
 
 function drawGame() {
     clearScreen();
+    snakePosition();
     drawSnake();
     setTimeout(drawGame, 1000 / speed)
 }
@@ -30,7 +31,40 @@ function drawSnake() {
     ctx.fillRect(headX*tileCount,headY*tileCount,tileSize,tileSize)
 }
 
+function snakePosition(){
+    headX=headX+xMove;
+    headY=headY+yMove
+}
 
-document.body.addEventListener('keydown',keyDown)
+
+document.body.addEventListener('keydown',keyDown);// keybord events
+
+function keyDown(event){
+    //keyCode 38 is Arrow Up
+    if(event.keyCode==38){
+        if(yMove==1)return //snake move only forward
+        yMove=-1;
+        xMove=0;
+    }
+    // keyCode 40 is Arrow down
+    if(event.keyCode==40){
+        if(yMove==-1)return
+        yMove=1;
+        xMove=0;
+    }
+   // keyCode 39 is Left Arrow
+   if(event.keyCode==39){
+    if(xMove==-1)return
+    yMove=0;
+    xMove=1;
+}
+  //keyCode 37 is right Arrow
+  if(event.keyCode==37){
+    if(xMove==1)return
+    yMove=0;
+    xMove=-1;
+}
+
+}
 
 drawGame()
